7cb6c0c5c84b967e2f5c137c9fb2ba8f9f05f74e
haristunner <haristunner10@gmail.com>
2022-12-25 23:20:45 +0530

    updated

--- src/calculator.js
+++ src/calculator.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./calculator.css";
+import { NumericFormat } from "react-number-format";
 
 export const Calculator = () => {
   const [input, setInput] = useState("0");
   const [prevState, setPrevState] = useState();
   const [currState, setCurrState] = useState();
   const [total, setTotal] = useState(false);
+  const [op, setOp] = useState();
 
   const inputNum = (e) => {
     if (e.target.innerText === "." && currState.includes(".")) return;
 
+    if (total) {
+      setPrevState("");
+    }
+
     currState
       ? setCurrState((prev) => prev + e.target.innerText)
       : setCurrState(e.target.innerText);
+    setTotal(false);
   };
 
   useEffect(() => {
@@ -23,16 +30,102 @@ export const Calculator = () => {
     setInput("0");
   }, []);
 
+  const deleteAll = (e) => {
+    setInput("0");
+    setCurrState("");
+    setPrevState("");
+  };
+
+  const operator = (e) => {
+    setTotal(false);
+
+    setOp(e.target.innerText);
+    if (currState === "") return;
+
+    if (prevState !== "") {
+      equals();
+    } else {
+      setPrevState(currState);
+      setCurrState("");
+    }
+  };
+
+  const equals = (e) => {
+    if (e.target.innerText === "=") setTotal(true);
+
+    let cal;
+
+    switch (op) {
+      case "/": {
+        cal = String(parseFloat(prevState) / parseFloat(currState));
+        break;
+      }
+
+      case "X": {
+        cal = String(parseFloat(prevState) * parseFloat(currState));
+        break;
+      }
+
+      case "+": {
+        cal = String(parseFloat(prevState) + parseFloat(currState));
+        break;
+      }
+
+      case "-": {
+        cal = String(parseFloat(prevState) - parseFloat(currState));
+        break;
+      }
+
+      default:
+        return;
+    }
+    setInput("");
+    setPrevState(cal);
+    setCurrState("");
+  };
+
+  const remove = (e) => {
+    setCurrState(currState.slice(0, -1));
+  };
+
+  const modulos = (e) => {
+    prevState
+      ? setCurrState(String((parseFloat(currState) / 100) * prevState))
+      : setCurrState(String(parseFloat(currState) / 100));
+  };
+
   return (
     <div className="calculator">
       <div className="container">
         <div className="all">
-          <div className="screen">{input}</div>
+          <div className="screen">
+            {input !== "" || input === "0" ? (
+              <NumericFormat
+                value={input}
+                displayType={"text"}
+                thousandSeparator={true}
+              />
+            ) : (
+              <NumericFormat
+                value={prevState}
+                displayType={"text"}
+                thousandSeparator={true}
+              />
+            )}
+          </div>
 
-          <div className="first">AC</div>
-          <div className="first">%</div>
-          <div className="first">+/-</div>
-          <div className="yellow">/</div>
+          <div className="first" onClick={deleteAll}>
+            AC
+          </div>
+          <div className="first" onClick={remove}>
+            C
+          </div>
+          <div className="first" onClick={modulos}>
+            %
+          </div>
+          <div className="yellow" onClick={operator}>
+            /
+          </div>
 
           <div className="second" onClick={inputNum}>
             7
@@ -43,7 +136,9 @@ export const Calculator = () => {
           <div className="second" onClick={inputNum}>
             9
           </div>
-          <div className="yellow">%</div>
+          <div className="yellow" onClick={operator}>
+            X
+          </div>
 
           <div className="second" onClick={inputNum}>
             4
@@ -54,7 +149,9 @@ export const Calculator = () => {
           <div className="second" onClick={inputNum}>
             6
           </div>
-          <div className="yellow">+</div>
+          <div className="yellow" onClick={operator}>
+            +
+          </div>
 
           <div className="second" onClick={inputNum}>
             1
@@ -65,7 +162,9 @@ export const Calculator = () => {
           <div className="second" onClick={inputNum}>
             3
           </div>
-          <div className="yellow">-</div>
+          <div className="yellow" onClick={operator}>
+            -
+          </div>
 
           <div className="second-0" onClick={inputNum}>
             0
@@ -73,7 +172,9 @@ export const Calculator = () => {
           <div className="second" onClick={inputNum}>
             .
           </div>
-          <div className="second">=</div>
+          <div className="second" onClick={equals}>
+            =
+          </div>
         </div>
       </div>
     </div>
